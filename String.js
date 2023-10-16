48591c22b36eb998514115208a30a2ce7c358104
sakin_dev@79 <107269051+Sakin790@users.noreply.github.com>
2023-10-16 17:24:37 +0000

    String file

---
+++ String.js
@@ -0,0 +1,11 @@
+let name = " Sakin Islam";
+let city = " Rajshahi";
+let phone = 1755446659;
+console.log(`im ${name}   livinng in ${city} my pnone number is ${phone}`);
+// shamne p pechone theke space badh dute hole 'trim' method use korbo
+
+const url = " Save%Palestine"; //% ==> mane holo space
+console.log(url.replace("Save", "-")); // %20 replace by "-"
+console.log(url.includes("Sakin")); // url string a kono property ache kina ta janar jonno
+
+console.log(String.prototype());
