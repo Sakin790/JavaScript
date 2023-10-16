7c9defacc6ab5bd8d7112570d3c9926112d18247
sakin_dev@79 <107269051+Sakin790@users.noreply.github.com>
2023-10-16 17:20:49 +0000

    math file

---
+++ Math.js
@@ -0,0 +1,7 @@
+let MathFun = Math;
+console.log(MathFun.random());
+console.log(MathFun.abs(-2)); // Eta input er poisitive value dibe
+console.log(MathFun.floor(4.9)); //eta input er ground value dibe
+console.log(MathFun.fround(5.1)); // Temon kaj er na
+console.log(MathFun.ceil(4.6)); // Kase upper value show korbe
+console.log(MathFun.random(10)); //output 0-1 anything
