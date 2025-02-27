7ed9df59e158a6c750b99cfe0d1161fa13ab91ad
Sakin790 <sakinislam79@gmail.com>
2025-02-26 21:24:24 -0800

    add functions to print numbers from 1 to n, even numbers, and odd numbers

---
+++ Loop/one_n.js
@@ -0,0 +1,18 @@
+function one_to_n(n) {
+  for (let i = 1; i <= n; i++) {
+    console.log(i);
+  }
+}
+
+function even() {
+  for (i = 2; i <= 100; i += 2) {
+    console.log(i);
+  }
+}
+
+function odd() {
+  for (i = 1; i <= 100; i += 2) {
+    console.log(i);
+  }
+}
+odd();
