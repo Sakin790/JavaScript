719ed4a823de460184b2724eb58b8c0a08bee2af
Sakin790 <sakinislam79@gmail.com>
2025-03-03 00:33:17 +0600

    add function to find the index of the middle element in an array

---
+++ Array/mid.js
@@ -0,0 +1,7 @@
+function middle(arr) {
+  return Math.floor((0 + arr.length - 1) / 2);
+}
+
+//it will return index value of the middle element of the array
+let result = middle([0, 3, 4, 7, 9, 12]);
+console.log(result);
