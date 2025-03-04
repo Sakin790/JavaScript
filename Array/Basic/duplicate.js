0ba40baec8f7ba66af0a29bdceb246dce86aa3b2
Sakin790 <sakinislam79@gmail.com>
2025-03-04 23:47:10 +0600

    remove duplicte from array using hashmap

--- Array/Basic/duplicate.js
+++ Array/Basic/duplicate.js
@@ -1,5 +1,17 @@
-function duplicate(array) {
-  let non_duplicate = [];
+function removeDuplicates(arr) {
+  let map = new Map();
+  let result = [];
 
-  array.forEach((element) => {});
+  for (let item of arr) {
+    if (!map.has(item)) {
+      map.set(item, true);
+      result.push(item);
+    }
+  }
+
+  return result;
 }
+
+// Example usage
+const arr = [1, 2, 3, 2, 4, 1, 5];
+console.log(removeDuplicates(arr)); // Output: [1, 2, 3, 4, 5]
