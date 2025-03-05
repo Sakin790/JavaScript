1563e757e3de218d4dd57d8441c2829bae7ddfd7
Sakin790 <sakinislam79@gmail.com>
2025-03-06 00:01:23 +0600

    bubble sort

--- Array/Bubble/swaping.js
+++ Array/Bubble/swaping.js
@@ -1,6 +1,9 @@
-//compare two element, adjust bigger elemenet in last
-
-let domoArray = [3, 5, 6, 2, 5, 1, 3];
+function swapTwoNumber(num1, num2) {
+  let temp = num1;
+  num1 = num2;
+  num2 = temp;
+  return [num1, num2];
+}
 
 function bubleSort(nums) {
   let n = nums.length;
@@ -8,12 +11,13 @@ function bubleSort(nums) {
   for (let i = 0; i < n - 1; i++) {
     for (let j = 0; j < n - i - 1; j++) {
       if (nums[j] > nums[j + 1]) {
-        [nums[j], nums[j + 1]] = [nums[j + 1], nums[j]];
+        [nums[j], nums[j + 1]] = swapTwoNumber(nums[j], nums[j + 1]);
       }
     }
   }
   return nums;
 }
 
+let domoArray = [3, 5, 6, 2, 5, 1, 3];
 let opt = bubleSort(domoArray);
 console.log(opt);
