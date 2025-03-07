62f71591968a67033e5aafbb7bd519b73effbdac
Sakin790 <sakinislam79@gmail.com>
2025-03-07 14:09:58 +0600

    swaping

---
+++ Array/Basic/swap.js
@@ -0,0 +1,7 @@
+let arr = [1, 2];
+[arr[0], arr[1]] = [arr[1], arr[0]];
+console.log(arr);
+
+let score = [98, 76, 34];
+[score[1], score[2]] = [score[2], score[1]];
+console.log(score);
--- Loop/while.js
+++ Loop/while.js
@@ -1,5 +1,3 @@
-let arr = new Array(2);
-
 let score = [12, 43, 54, 13];
 
 let i = 0;
