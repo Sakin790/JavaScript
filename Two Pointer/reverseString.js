945aebc91485887ba7ec4575a74747761b8e2b8e
Sakin790 <sakinislam79@gmail.com>
2025-03-11 00:35:46 +0600

    add reverse string function and remove palindrome check

---
+++ Two Pointer/reverseString.js
@@ -0,0 +1,11 @@
+var reverseString = function (s) {
+  let left = 0,
+    right = s.length - 1;
+
+  while (left < right) {
+    [s[left], s[right]] = [s[right], s[left]];
+    left++;
+    right--;
+  }
+};
+reverseString(["a", "b", "c"]);
