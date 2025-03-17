79bbf43940a030abb7ce90b567e0b126a7d7a518
Sakin790 <sakinislam79@gmail.com>
2025-03-18 01:31:50 +0600

    m

--- Number/index.js
+++ Number/index.js
@@ -1,6 +1,3 @@
-var rotateString = function (s, goal) {
-  let arr1 = s.split("");
-  let arr2 = goal.split("");
-  if (arr1.length !== arr2.length) return false;
-  return arr1.every((value, index) => value === arr2[index]);
+var separateDigits = function (nums) {
+  return nums.join("").split("").map(Number);
 };
