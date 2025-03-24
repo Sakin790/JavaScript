977b61d38c9fb842d53e944aedd71ef083e1ebb3
Sakin790 <sakinislam79@gmail.com>
2025-03-24 15:52:15 +0600

    recursion is not hard

---
+++ Recursion/Factorial.java
@@ -0,0 +1,18 @@
+public class Factorial {
+    public static int factorial(int n) {
+        if (n == 0) {
+            return 1;
+        }
+        System.out.println(n);
+        return n * factorial(n - 1);
+    }
+
+    public static void main(String[] args) {
+        int result = factorial(5);
+        System.out.println("Factorial: " + result);
+    }
+}
+
+// first call main function with params 5
+// then if condition check, n is 0 or not
+// if n = 0, return 1,
---
+++ Recursion/PrintArray.java
@@ -0,0 +1,14 @@
+public class PrintArray {
+    public static void printarr(int[] arr, int index) {
+
+        if (index < 0)
+            return;
+        System.out.println(arr[index] + " ");
+        printarr(arr, index - 1);
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 4, 5 };
+        printarr(arr, arr.length - 1);
+    }
+}
